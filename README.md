0f62217b291f5b329a81073f9acecfc60989a5e9
phnz <hello@popcorntime.io>
2014-12-16 08:51:38 -0500

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# openvpn builder
+
+This scripts is grabing our latest openvpn for the distro and generate a tar with everything needed.
--- scripts/openvpn.js
+++ scripts/openvpn.js
@@ -4,6 +4,7 @@ var fs = require('fs-plus');
 var mv = require('mv');
 var zlib = require('zlib');
 var path = require('path');
+var fstream = require("fstream");
 
 var request = null;
 request = require('../lib/request');
@@ -21,7 +22,7 @@ var config = {
 	},
 	darwin: {
 		x86: 'https://github.com/VPNht/node-builder/releases/download/openvpn/openvpn-mac.tar.gz',
-		x64: 'https://github.com/VPNht/node-builder/releases/download/openvpn/openvpn-windows-x64.exe',
+		x64: 'https://github.com/VPNht/node-builder/releases/download/openvpn/openvpn-mac.tar.gz',
 	},
 	linux: {
 		x86: 'https://github.com/VPNht/node-builder/releases/download/openvpn/openvpn-linux-x86.tar.gz',
@@ -31,7 +32,7 @@ var config = {
 
 var downloadFileToLocation = function (url, filename, callback) {
 	var stream = fs.createWriteStream(filename);
-	stream.on('end', callback);
+	stream.on('finish', callback);
 	stream.on('error', callback);
 	request.createReadStream({
 		url: url
@@ -69,40 +70,87 @@ var downloadOpenVPN = function (done) {
 	arch = process.arch === 'ia32' ? 'x86' : process.arch;
 
 	// we clean our download folder
-	fs.removeSync(path.resolve(__dirname, '..', 'openvpn'));
+	fs.removeSync('openvpn');
 
 	downloadURL = config[process.platform][arch];
 
 	if (process.platform === 'win32') {
 		// if windows we need our dir so we'll create it
-		fs.makeTreeSync(path.resolve(__dirname, '..','openvpn'));
+		fs.makeTreeSync('openvpn');
 		filename = path.join('openvpn', "openvpn.exe");
 	} else {
 		// we tell him to extract in openvpn folder
 		filename = path.join('openvpn');
 	}
 
-	var downloadFile = function () {
-		// it's a EXE
-		if (process.platform === 'win32') {
-			downloadFileToLocation(downloadURL, filename, done);
-		} else if (process.platform === 'linux') {
-			var next = copyToLocation.bind(this, done, filename);
-			downloadTarballAndExtract(downloadURL, filename, next);
-		} else {
-			var next = copyToLocation.bind(this, done, filename);
-			downloadTarballAndExtract(downloadURL, filename, next);
-		}
-	};
-
-	downloadFile();
+	// it's a EXE
+	if (process.platform === 'win32') {
+		downloadFileToLocation(downloadURL, filename, done);
+	} else if (process.platform === 'linux') {
+		var next = copyToLocation.bind(this, done, filename);
+		downloadTarballAndExtract(downloadURL, filename, next);
+	} else {
+		var next = copyToLocation.bind(this, done, filename);
+		downloadTarballAndExtract(downloadURL, filename, next);
+	}
 };
+
+var createTarOpenVpn = function (done) {
+
+	var dirDest = fs.createWriteStream('build/openvpn.tar');
+
+	// we'll copy our openvpn.conf
+	fs.createReadStream('openvpn.conf').pipe(fs.createWriteStream('openvpn/openvpn.conf'));
+
+	var packer = tar.Pack({ noProprietary: true })
+	  .on('error', onError)
+	  .on('end', done);
+
+	fstream.Reader({ path: path.resolve(__dirname, '..', 'openvpn'), type: "Directory" })
+	  .on('error', onError)
+	  .pipe(packer)
+	  .pipe(dirDest)
+}
+
+var createTarRunAs = function (done) {
+
+	var dirDest = fs.createWriteStream('build/runas.tar');
+
+	var packer = tar.Pack({ noProprietary: true })
+	  .on('error', onError)
+	  .on('end', done);
+
+	fstream.Reader({ path: path.resolve(__dirname, '..', 'node_modules', 'runas'), type: "Directory" })
+	  .on('error', onError)
+	  .pipe(packer)
+	  .pipe(dirDest)
+}
+
+var onError = function (err) {
+  console.error('An error occurred:', err)
+}
 
 downloadOpenVPN(function (error) {
 	if (error != null) {
 		console.error('Failed to download openvpn', error);
 		return process.exit(1);
 	} else {
-		return process.exit(0);
+		// we can pack it !
+		createTarOpenVpn(function(error) {
+			if (error != null) {
+				console.error('Failed to generate final build', error);
+				return process.exit(1);
+			} else {
+				// create our runas tar
+				createTarRunAs(function(error) {
+					if (error != null) {
+						console.error('Failed to generate final build', error);
+						return process.exit(1);
+					} else {
+						return process.exit(0);
+					}
+				})
+			}
+		})
 	}
 });
